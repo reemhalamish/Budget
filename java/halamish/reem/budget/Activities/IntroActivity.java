ae0f94c61a39f0de88f5c2ac2831f048c4a8b359
reemhalamish <reem.halamish@gmail.com>
2016-02-28 14:46:55 +0200

    fixed bug language-bubbles on top of each-other at introActivity

--- java/halamish/reem/budget/Activities/IntroActivity.java
+++ java/halamish/reem/budget/Activities/IntroActivity.java
@@ -93,107 +93,7 @@ public class IntroActivity extends BudgetFullScreenActivity implements FirstActi
         }
     }
 
-    /**
-     * Dispatch onResume() to fragments.  Note that for better inter-operation
-     * with older versions of the platform, at the point of this call the
-     * fragments attached to the activity are <em>not</em> resumed.  This means
-     * that in some cases the previous state may still be saved, not allowing
-     * fragment transactions that modify the state.  To correctly interact
-     * with fragments in their proper state, you should instead override
-     * {@link #onResumeFragments()}.
-     */
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
 
-//    @Override
-//    public void onCloseWhenReady() {
-//        finish();
-//        overridePendingTransition(0,0); // exit without showing any animation
-//    }
-
-//    @Override
-//    /**
-//     * show the language buttons, invisiblate the other bubbles and start TutorialActivity if pressing
-//     */
-//    public void onContinueYourself() {
-//        activityGotContinue = true;
-//        final ImageView iv_bubble2;
-//
-//        tvEng =         (TextView)  findViewById(R.id.tv_intro_bubble1_eng);
-//        iv_bubble2 =    (ImageView) findViewById(R.id.iv_intro_bubble2);
-//        tvHeb =         (TextView)  findViewById(R.id.tv_intro_bubble3_heb);
-//        rl_intro = (RelativeLayout) findViewById(R.id.rl_intro);
-//
-//        // Log.d(TAG, "onContinueYourself()");
-//        // Log.d(TAG, "left: " + tvEng.getLeft());
-//
-//
-//        // set the font
-//        Typeface hebrewFont, englishFont, defaultFont;
-//        hebrewFont = Typeface.createFromAsset(getApplication().getAssets(), Settings.Language.HEBREW_UNISEX.getFontYoung());
-//        englishFont = Typeface.createFromAsset(getApplication().getAssets(), Settings.Language.ENGLISH.getFontYoung());
-//        defaultFont = Settings.getInstance().getFont();
-//        tvEng.setTypeface(englishFont);
-//        tvHeb.setTypeface(hebrewFont);
-//        ((TextView)findViewById(R.id.tv_intro_line)).setTypeface(defaultFont);
-//        findViewById(           R.id.tv_intro_line).setVisibility(View.VISIBLE);
-//
-//        moveBubbles(true);
-//
-//        iv_bubble2.animate()
-//                .alpha(0.0f)
-//                .setListener(new Animator.AnimatorListener() {
-//                    @Override
-//                    public void onAnimationStart(Animator animator) {
-//
-//                    }
-//
-//                    @Override
-//                    public void onAnimationEnd(Animator animator) {
-//                        iv_bubble2.setVisibility(View.INVISIBLE);
-//                    }
-//
-//                    @Override
-//                    public void onAnimationCancel(Animator animator) {
-//
-//                    }
-//
-//                    @Override
-//                    public void onAnimationRepeat(Animator animator) {
-//
-//                    }
-//                }); // invisible at the end
-//
-//        // set click listeners to start the tutorial
-//        final Intent startTutorial = new Intent(this, TutorialActivity.class);
-//        startTutorial.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-//
-//        tvEng.setOnClickListener(
-//                new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View view) {
-//                        Settings.getInstance().setUserLanguage(Settings.Language.ENGLISH);
-//                        finish();
-//                        startActivity(startTutorial);
-//                    }
-//                }
-//        );
-//
-//        tvHeb.setOnClickListener(
-//                new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View view) {
-//                        Settings.getInstance().setUserLanguage(Settings.Language.HEBREW_UNISEX);
-//                        finish();
-//                        startActivity(startTutorial);
-//                    }
-//                }
-//        );
-//
-//    }
-//
     private void moveBubbles() {
         final TextView tvHeb, tvEng;
         final ImageView ivBubble2;
@@ -204,20 +104,20 @@ public class IntroActivity extends BudgetFullScreenActivity implements FirstActi
         ivBubble2 =     (ImageView) findViewById(R.id.iv_intro_bubble2);
         rl_intro = (RelativeLayout) findViewById(R.id.rl_intro);
 
-        float parentTop = rl_intro.getTop() + BUBBLE_MARGIN;
-        float parentBot = rl_intro.getBottom() - BUBBLE_MARGIN;
-        float parentLeft = rl_intro.getLeft() + BUBBLE_MARGIN;
-        float parentRight = rl_intro.getRight() - BUBBLE_MARGIN;
+        float parentTop = rl_intro.getTop();// + BUBBLE_MARGIN;
+        float parentBot = rl_intro.getBottom();// - BUBBLE_MARGIN;
+        float parentLeft = rl_intro.getLeft();// + BUBBLE_MARGIN;
+        float parentRight = rl_intro.getRight();// - BUBBLE_MARGIN;
 
-        float bubble1midX = tvEng.getX() + tvEng.getWidth()/2;
-        float bubble1midY = tvEng.getY() + tvEng.getHeight()/2;
+        float bubble1right = tvEng.getX() + tvEng.getWidth();
+        float bubble1top = tvEng.getY();
 
-        float bubble3midX = tvHeb.getX() + tvHeb.getWidth()/2;
-        float bubble3midY = tvHeb.getY() + tvHeb.getHeight()/2;
+        float bubble3left = tvHeb.getX();
+        float bubble3top = tvHeb.getY();
 
             tvEng.animate()
-                    .translationX(parentRight - bubble1midX)
-                    .translationY(parentTop - bubble1midY)
+                    .translationX(parentRight - bubble1right)
+                    .translationY(parentTop - bubble1top)
                     .setListener(new Animator.AnimatorListener() {
                         @Override
                         public void onAnimationStart(Animator animator) {
@@ -243,8 +143,8 @@ public class IntroActivity extends BudgetFullScreenActivity implements FirstActi
                     .setDuration(BUBBLE_ANIM_DUR_MS);
 
             tvHeb.animate()
-                    .translationX(parentLeft - bubble3midX)
-                    .translationY(parentTop - bubble3midY)
+                    .translationX(parentLeft - bubble3left)
+                    .translationY(parentTop - bubble3top)
                     .setListener(new Animator.AnimatorListener() {
                         @Override
                         public void onAnimationStart(Animator animator) {
